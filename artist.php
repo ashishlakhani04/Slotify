dfc49d02a0189b6a9234e762117424d5e48769e4
Ashish Lakhani <depashlakhani@gmail.com>
2018-04-06 13:59:27 +0530

    Add artist page

---
+++ artist.php
@@ -0,0 +1,96 @@
+<?php
+
+	include "includes/includedFiles.php";
+
+	if(isset($_GET['id'])){
+		$artistId = $_GET['id'];
+	}else{
+		header("Location: index.php");
+	}
+
+	$artist = new Artist($con,$artistId);
+
+?>
+
+<div class="entityInfo borderBottom">
+
+	<div class="centerSection">
+		 	
+		 <div class="artistInfo">
+		 	
+		 	<h1 class="artistName"><?php echo $artist->getName(); ?></h1>
+
+		 	<div class="headerButtons">
+
+		 		<button class="button green">PLAY</button>
+		 		
+		 	</div>
+
+		 </div>
+
+	</div>
+	 
+
+</div>
+<div class="tracklistContainer borderBottom">
+
+	<ul class="tracklist">
+
+		<?php  
+
+			$songIdArray = $artist->getSongIds();
+			$i=1;
+
+			foreach ($songIdArray as $songId) {
+
+				if($i > 5){
+					break;
+				}
+
+				$albumSong = new Song($con,$songId);
+
+				$albumArtist = $albumSong->getArtist();
+
+				echo "<li class='tracklistRow'>
+
+						<div class='trackCount'>
+							<img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"". $albumSong->getId() ."\",tempPlaylist,true)'>
+							<span class='trackNumber'>".$i++."</span>
+
+						</div>
+
+						<div class='trackInfo'>
+
+							<span class='trackName'>".$albumSong->getTitle()."</span>
+
+							<span class='artistName'>".$albumArtist->getName()."</span>
+
+						</div>
+						<div class='trackOptions'>
+
+							<img class='optionButton' src='assets/images/icons/more.png'>
+
+						</div>
+
+						<div class='trackDuration'>
+
+							<span class='duration'>".$albumSong->getDuration()."</span>
+
+						</div>
+
+					</li>";
+				
+			}
+
+		?>
+
+		<script>
+			
+			var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+			tempPlaylist = JSON.parse(tempSongIds);
+
+		</script>
+		
+	</ul>
+
+</div>
--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -15,6 +15,18 @@
 			return $artist['name'];
 		}
 
+		public function getSongIds(){
+			$query = mysqli_query($this->con,"SELECT id FROM Songs WHERE artist='$this->id' ORDER BY plays DESC ");
+			$array = array();
+
+			while($row = mysqli_fetch_array($query)){
+				array_push($array,$row['id']);
+			}
+
+			return $array;
+
+		}
+
 
 	}
 ?>
